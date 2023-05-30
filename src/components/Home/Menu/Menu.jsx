a26aa9d8e339132190bd433380e31cf3caad4ae5
jubair-jx <jubair.official97@gmail.com>
2023-05-30 22:32:12 +0600

    Some Feature Done

--- src/components/Home/Menu/Menu.jsx
+++ src/components/Home/Menu/Menu.jsx
@@ -13,16 +13,18 @@ const Menu = () => {
       });
   }, []);
   return (
-    <div className="mx-auto">
+    <div className="">
       <SectionTitle subHeading="---Check it out---" heading="FROM OUR MENU" />
       <div className="grid md:grid-cols-2 gap-10 mt-5 px-16 py-10">
         {popularMenu.map((item) => (
           <ShowMenuItem item={item}></ShowMenuItem>
         ))}
       </div>
-      <button className="btn text-center mx-auto btn-outline border-0 border-b-4 mt-4">
-        View Full Menu
-      </button>
+      <div className="flex justify-center items-center mb-6">
+        <button className="btn text-center mx-auto btn-outline border-0 border-b-4 mt-4">
+          View Full Menu
+        </button>
+      </div>
     </div>
   );
 };
