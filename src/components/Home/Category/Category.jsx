def72b4896d6a4d7fd6c2405df4db74264466b08
jubair-jx <jubair.official97@gmail.com>
2023-05-30 10:26:11 +0600

    Common Section title Done

---
+++ src/components/Home/Category/Category.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import SectionTitle from "../../Shared/SectionTitle/SectionTitle";
+
+const Category = () => {
+  return (
+    <div>
+      <SectionTitle
+        subHeading="From 11:00am to 10:00pm"
+        heading="Order Online"
+      ></SectionTitle>
+    </div>
+  );
+};
+
+export default Category;
---
+++ src/components/Shared/SectionTitle/SectionTitle.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const SectionTitle = ({ subHeading, heading }) => {
+  return (
+    <div className="text-center mx-auto">
+      <p className="text-yellow-500">---{subHeading}---</p>
+      <h2 className="uppercase text-3xl mx-auto mt-2 font-medium border-y-[3px] w-72 py-3  mb-3">
+        {heading}
+      </h2>
+    </div>
+  );
+};
+
+export default SectionTitle;
