f125ab6534d96b15e524e7e261107024c548c071
jubair-jx <jubair.official97@gmail.com>
2023-05-31 23:25:06 +0600

    Menu Category Show Done

--- src/components/Pages/Menu/MenuCategory.jsx
+++ src/components/Pages/Menu/MenuCategory.jsx
@@ -6,6 +6,7 @@ import ShowMenuItem from "../../Home/Menu/ShowMenuItem";
 const MenuCategory = ({ items, title, img }) => {
   return (
     <div className="pt-8">
+      {/*THis is Menu Category*/}
       {title && <Cover img={img} title={title}></Cover>}
       <div className="grid md:grid-cols-2 gap-10 my-16">
         {items.map((item) => (
