2edad5a3f66e94f0e87387cbbc56236090ac74be
jubair-jx <jubair.official97@gmail.com>
2023-06-02 11:13:42 +0600

    chaptcha inplement done

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -3,6 +3,7 @@ import Layout from "../Layout/Layout";
 import Home from "../components/Home/Home/Home";
 import Menu from "../components/Pages/Menu/Menu";
 import Order from "../components/Pages/Order/Order";
+import Login from "../components/Pages/Login/Login";
 
 const router = createBrowserRouter([
   {
@@ -20,6 +21,10 @@ const router = createBrowserRouter([
       {
         path: "order/:category",
         element: <Order></Order>,
+      },
+      {
+        path: "login",
+        element: <Login></Login>,
       },
     ],
   },
