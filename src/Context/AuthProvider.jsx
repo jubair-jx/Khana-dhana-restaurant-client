3ac11955f63660bf1462349cf26c080d60de19af
jubair-jx <jubair.official97@gmail.com>
2023-06-05 22:57:29 +0600

    JWT secure APis done

--- src/Context/AuthProvider.jsx
+++ src/Context/AuthProvider.jsx
@@ -13,6 +13,7 @@ import {
   updateProfile,
 } from "firebase/auth";
 import { useEffect } from "react";
+import axios from "axios";
 
 const auth = getAuth(app);
 export const AuthContext = createContext(null);
@@ -48,9 +49,21 @@ const AuthProvider = ({ children }) => {
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
       setUsers(currentUser);
-      // console.log("current user", currentUser);
+      console.log("current user", currentUser);
       setLoading(false);
+      if (currentUser) {
+        axios
+          .post("http://localhost:5000/jwt", { email: currentUser.email })
+          .then((data) => {
+            console.log(data.data);
+            localStorage.setItem("access-token", data.data.token);
+            setLoading(false);
+          });
+      } else {
+        localStorage.removeItem("access-token");
+      }
     });
+    setLoading(false);
     return () => {
       return unsubscribe();
     };
--- src/hooks/useCart.jsx
+++ src/hooks/useCart.jsx
@@ -2,14 +2,21 @@ import { useQuery } from "@tanstack/react-query";
 import { useContext } from "react";
 import { AuthContext } from "../Context/AuthProvider";
 const useCart = () => {
-  const { users } = useContext(AuthContext);
-  console.log(users);
-
+  const { users, loading } = useContext(AuthContext);
+  const token = localStorage.getItem("access-token");
+  // console.log(users.email);
   const { refetch, data: cart = [] } = useQuery({
     queryKey: ["cart", users?.email],
+    enabled:
+      !loading && !!users?.email && !!localStorage.getItem("access-token"),
     queryFn: async () => {
       const res = await fetch(
-        `http://localhost:5000/carts?email=${users?.email}`
+        `http://localhost:5000/carts?email=${users?.email}`,
+        {
+          headers: {
+            authorization: `bearer ${token}`,
+          },
+        }
       );
       return res.json();
     },
